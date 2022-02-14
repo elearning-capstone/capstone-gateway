eb464fdca864afcdd81f32cf3e8563cb303e1d13
kttkkkng <teerawat7971@gmail.com>
2022-02-14 08:40:47 +0700

    token

--- routes/user.js
+++ routes/user.js
@@ -8,12 +8,12 @@ const user_ip = "http://i-09a9471f1fb723e78.ap-southeast-1.compute.internal:3000
 const tokenVerifyMiddleware = async (req, res, next) => {
     const token = (req.headers['authorization'] || '').split('Bearer ')[1];
     try {
-        var user_id = jwt.verify(token, 'secret');
+        var user = jwt.verify(token, 'secret');
     } catch (err) {
         return res.status(403).json({ message: err.name });
     }
 
-    req.user_id = user_id;
+    req.user = user;
     next();
 }
 
@@ -22,16 +22,17 @@ router.post("/register", async (req, res) => {
         const response = await axios.post(user_ip + "/register", req.body);
         return res.json({ user: response.data.user });
     } catch (err) {
-        return res.status(err.response.status).json(err.response.data)
+        return res.status(err.response.status).json(err.response.data);
     }
 });
 
 router.post("/login", async (req, res) => {
     try {
         const response = await axios.post(user_ip + "/login", req.body);
-        return jwt.sign(response.data.user_id, "secret", { expiresIn: '7d' });
+        const token = jwt.sign({ user_id: response.data.user_id }, "secret", { expiresIn: "7d" });
+        return res.json({ token });
     } catch (err) {
-        return res.status(err.response.status).json(err.response.data)
+        return res.status(err.response.status).json(err.response.data);
     }
 });
 
