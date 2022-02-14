805b811e352b30cbb7c38fd3b2f68434cb1b1fb6
kttkkkng <teerawat7971@gmail.com>
2022-02-14 09:20:40 +0700

    middleware

---
+++ middleware.js
@@ -0,0 +1,11 @@
+export const tokenVerifyMiddleware = async (req, res, next) => {
+    const token = (req.headers['authorization'] || '').split('Bearer ')[1];
+    try {
+        var user = jwt.verify(token, 'secret');
+    } catch (err) {
+        return res.status(403).json({ message: err.name });
+    }
+
+    req.user = user;
+    next();
+};
--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,10 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const axios = require("axios");
+const { tokenVerifyMiddleware } = require("../middleware");
 const router = express.Router();
 
 const user_ip = "http://i-09a9471f1fb723e78.ap-southeast-1.compute.internal:3000";
-
-const tokenVerifyMiddleware = async (req, res, next) => {
-    const token = (req.headers['authorization'] || '').split('Bearer ')[1];
-    try {
-        var user = jwt.verify(token, 'secret');
-    } catch (err) {
-        return res.status(403).json({ message: err.name });
-    }
-
-    req.user = user;
-    next();
-}
 
 router.post("/register", async (req, res) => {
     try {
