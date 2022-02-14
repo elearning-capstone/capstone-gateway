fd782388bf53d4112427752244d59cda79176b0b
kttkkkng <teerawat7971@gmail.com>
2022-02-14 09:36:10 +0700

    middleware tested

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
-export const tokenVerifyMiddleware = async (req, res, next) => {
+const jwt = require("jsonwebtoken");
+
+exports.tokenVerifyMiddleware = async (req, res, next) => {
     const token = (req.headers['authorization'] || '').split('Bearer ')[1];
     try {
         var user = jwt.verify(token, 'secret');
