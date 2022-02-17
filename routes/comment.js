35bbed3d535da1e90b9e4d02f1b68a123d71b48c
kttkkkng <teerawat7971@gmail.com>
2022-02-17 21:53:57 +0700

    init comment

---
+++ routes/comment.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const axios = require("axios");
+const { tokenVerifyMiddleware } = require("../middleware");
+const router = express.Router();
+
+const user_ip = "http://ip-172-31-33-253.ap-southeast-1.compute.internal:3000";
+
+router.get("/comment", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        console.log(req.query);
+        const response = await axios.get(user_ip + "/comment", { headers: { user: req.headers['user'] }, params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+module.exports = router;
