d1e9d890f7476113668e33443f0a94b519735b0a
noof3310 <noof3310@gmail.com>
2022-02-18 12:50:48 +0700

    update route comment

--- routes/comment.js
+++ routes/comment.js
@@ -5,10 +5,46 @@ const router = express.Router();
 
 const user_ip = "http://ip-172-31-33-253.ap-southeast-1.compute.internal:3000";
 
-router.get("/comment", tokenVerifyMiddleware, async (req, res) => {
+router.get("/", tokenVerifyMiddleware, async (req, res) => {
     try {
         console.log(req.query);
         const response = await axios.get(user_ip + "/comment", { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/comment", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/reply", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/comment/reply", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/like", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/comment/like", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/unlike", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/unlike", req.body, { params: req.query });
         return res.json(response.data);
     } catch (err) {
         return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
