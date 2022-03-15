db9db1758489965a1c11546f6737b7081613df76
kttkkkng <teerawat7971@gmail.com>
2022-03-15 20:54:57 +0700

    survey routes

---
+++ routes/survey.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+const { tokenVerifyMiddleware } = require("../middleware");
+const router = express.Router();
+
+const user_ip = "http://ip-172-31-33-253.ap-southeast-1.compute.internal:3000";
+
+router.get("/", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.get(user_ip + "/survey", { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/survey", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+module.exports = router;
