f940bb625ca43180843dc84d84c75b6c9bc9e4ba
kttkkkng <teerawat7971@gmail.com>
2022-05-08 02:26:35 +0700

    common routes live

---
+++ routes/live.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const axios = require("axios");
+const { tokenVerifyMiddleware } = require("../middleware");
+const router = express.Router();
+
+const user_ip = "http://ip-172-31-33-253.ap-southeast-1.compute.internal:3000";
+
+router.post("/survey", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/survey/live_survey", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.get("/sync", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.get(user_ip + "/live/sync", { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/chat", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/live/chat", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(user_ip + "/live/", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.put("/info", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.put(user_ip + "/live/info", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+module.exports = router;
