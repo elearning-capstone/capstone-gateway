16ee796b3c5447e957123e1feb477b72310031a9
noof3310 <noof3310@gmail.com>
2022-05-10 22:31:41 +0700

    add route get live

--- routes/live.js
+++ routes/live.js
@@ -5,6 +5,15 @@ const router = express.Router();
 
 const user_ip = "http://ip-172-31-33-253.ap-southeast-1.compute.internal:3000";
 
+router.get("/", tokenVerifyMiddleware, async (req, res) => {
+    try {
+        const response = await axios.get(user_ip + "/live", { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
 router.post("/survey", tokenVerifyMiddleware, async (req, res) => {
     try {
         const response = await axios.post(user_ip + "/live/survey", req.body, { params: req.query });
